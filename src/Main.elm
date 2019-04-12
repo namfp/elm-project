550d3fe5889ae1d348e33d5b39caf95f241cb8c3
Hoai-Nam NGUYEN <Hoai-nam.NGUYEN@ingenico.com>
2019-04-12 16:58:45 +0200

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,114 @@
+module Main exposing (Board, Cell(..), Model, Msg(..), evolve, init, lookForNeighborhoods, main, update, view)
+
+import Array exposing (Array)
+import Array
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
+type Cell
+    = Dead
+    | Alive
+
+
+type alias Board =
+    Array (Array Cell)
+
+
+evolve : Cell -> List Cell -> Cell
+evolve cell neighborhoods =
+    let
+        size =
+            List.length neighborhoods
+    in
+    case ( cell, size ) of
+        ( Alive, x ) ->
+            if x < 2 then
+                Dead
+
+            else if x == 2 || x == 3 then
+                Alive
+
+            else
+                Dead
+
+        ( Dead, x ) ->
+            if x == 3 then
+                Alive
+
+            else
+                cell
+
+
+lookForNeighborhoods : Int -> Int -> Board -> Result String (List Cell)
+lookForNeighborhoods x y board =
+    let
+        positions =
+            [ ( x - 1, y - 1 )
+            , ( x - 1, y )
+            , ( x - 1, y + 1 )
+            , ( x , y + 1 )
+            , ( x , y - 1 )
+            , ( x + 1 , y - 1 )
+            , ( x + 1, y )
+            , ( x + 1, y + 1 )
+            ]
+
+        width = (Array.get 0 board) |> Result.fromMaybe ("Board must not be empty") |> Result.map Array.length
+        height = Array.length board
+
+        filter (x, y) = x >= 0 && x <
+
+        filteredPositions = positions |> List.filter ()
+    in
+    [ Dead ]
+
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
 
 
 -- MODEL
 
-type alias Model = Int
+
+type alias Model =
+    Int
+
 
 init : Model
 init =
-  0
+    0
+
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
