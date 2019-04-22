579b5aadf7320a13e98a3b89413d48b0270bb195
Nam <namnguyen107@gmail.com>
2019-04-22 16:28:33 +0200

    almost works

--- src/TicTacToe.elm
+++ src/TicTacToe.elm
@@ -1,6 +1,10 @@
 module TicTacToe exposing (Cell(..), Game, Player(..), evaluate)
 
 import Array exposing (Array)
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Tools
 
 
@@ -85,6 +89,7 @@ nextPlayer player =
             Self
 
 
+
 allPossibleMoves : Game -> List Game
 allPossibleMoves game =
     let
@@ -150,14 +155,15 @@ evaluate currentDepth game =
                 possibleMoves : List Game
                 possibleMoves =
                     allPossibleMoves game
-                evaluatedMoves = possibleMoves |> List.map (evaluate (currentDepth + 1))
+
+                evaluatedMoves =
+                    possibleMoves |> List.map (evaluate (currentDepth + 1))
 
                 scored : Maybe GameState
                 scored =
                     case game.player of
                         Self ->
                             evaluatedMoves |> Tools.maxBy .score
-
                         Opponent ->
                             evaluatedMoves |> Tools.minBy .score
 
@@ -165,3 +171,135 @@ evaluate currentDepth game =
                     scored |> Maybe.map (\s -> { score = s.score, originalGame = game, nextMove = Just s.originalGame })
             in
             Maybe.withDefault { score = 0, originalGame = game, nextMove = Nothing } result
+
+
+
+--Message
+
+
+type Msg
+    = Play Int Int
+
+
+
+-- View
+
+
+renderCell mark i j =
+    td
+        [ style "backgroundColor" "white"
+        , style "border" "3px solid black"
+        , style "font-size" "100px"
+        , style "color" "black"
+        , style "border-radius" "2px 2px 2px 2px"
+        , height 150
+        , width 150
+        , align "center"
+        , style "valign" "center"
+        , onClick (Play i j)
+        ]
+        [ text mark ]
+
+
+renderGame : Array (Array Cell) -> Html Msg
+renderGame board =
+    let
+        renderLine j line =
+            line
+                |> Array.indexedMap
+                    (\i v ->
+                        case v of
+                            Played Self ->
+                                renderCell "X" i j
+
+                            Played Opponent ->
+                                renderCell "O" i j
+
+                            NotPlayed ->
+                                renderCell "" i j
+                    )
+                |> Array.toList
+                |> tr []
+
+        renderBoard =
+            board
+                |> Array.indexedMap
+                    (\j line ->
+                        renderLine j line
+                    )
+                |> Array.toList
+                |> table
+                    [ style "border-collapse" "collapse"
+                    , style "border" "1"
+                    , style "cellspacing" "0"
+                    ]
+    in
+    renderBoard
+
+
+view : Game -> Html Msg
+view game =
+    renderGame game.board
+
+
+
+--Update
+
+
+update : Msg -> Game -> Game
+update msg game =
+    case ( msg, game ) of
+        ( Play i j, { board, player } ) ->
+            if player == Opponent then
+                let
+                    currentLine =
+                        Array.get j game.board
+
+                    updatedCell : Maybe Cell
+                    updatedCell =
+                        currentLine
+                            |> Maybe.andThen (Array.get i)
+                            |> Maybe.map
+                                (\cell ->
+                                    case cell of
+                                        NotPlayed ->
+                                            Played Opponent
+
+                                        current ->
+                                            current
+                                )
+
+                    ( updatedBoard, updatedPlayer ) =
+                        case ( currentLine, updatedCell ) of
+                            ( Just line, Just c ) ->
+                                ( Array.set j (Array.set i c line) board, nextPlayer player )
+
+                            _ ->
+                                ( board, player )
+                    updatedGame = { game | board = updatedBoard, player = updatedPlayer }
+                    gameValue = evaluate 0 updatedGame
+
+                in
+                Maybe.withDefault game gameValue.nextMove
+
+            else
+                game
+
+
+
+-- Main
+
+
+initGame =
+    { board =
+        Array.fromList
+            [ Array.fromList [ NotPlayed, NotPlayed, NotPlayed ]
+            , Array.fromList [ NotPlayed, NotPlayed, NotPlayed ]
+            , Array.fromList [ NotPlayed, NotPlayed, NotPlayed ]
+            ]
+    , player = Opponent
+    }
+
+
+main =
+    Browser.sandbox { init = initGame, view = view, update = update }
